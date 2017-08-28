44d5ab1bd0ac036bfe4497f3261612253cad32a6
EddieKeller <116ekg@gmail.com>
2017-08-28 14:18:10 -0700

    added route for currency details page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,7 @@ import {
 import LandingPage from './LandingPage.jsx'
 import axios from 'axios'
 import PortfolioPage from './Simulator/PortfolioPage.js'
+import CurrInfo from './CurrInfo.jsx'
 
 
 export default class App extends React.Component {
@@ -21,6 +22,7 @@ export default class App extends React.Component {
         <div>
           <Route exact path='/' render={() => <LandingPage />} />
           <Route path='/simulator' render={() => <PortfolioPage />} />
+          <Route path='/currency/*' render={() => <CurrInfo />} />
         </div>
 
       </Router>
---
+++ client/components/CurrInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class CurrInfo extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+
+      <div>
+        <h1>Hello world</h1>
+      </div>
+
+    )
+  }
+}
