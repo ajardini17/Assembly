129642a9204ab585448af600e88e075101b7abbb
EddieKeller <116ekg@gmail.com>
2017-08-30 14:12:25 -0700

    basic ML implmented

--- server/python/ccGetData.py
+++ server/python/ccGetData.py
@@ -67,11 +67,34 @@ def compile_closes_data():
 
     main_df.to_csv('crypto_history_joined_closes.csv')
 
-compile_closes_data()
+# compile_closes_data()
 
 def visualize_data():
-    df = pd.read_csv('cc_dfs/historyBTC.csv')
-    df['close'].plot()
+    df = pd.read_csv('crypto_history_joined_closes.csv')
+    # df['BTC'].plot()
+    # plt.show()
+
+    df_corr = df.corr()
+
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1, 1)
+    plt.tight_layout()
     plt.show()
 
 # visualize_data()
