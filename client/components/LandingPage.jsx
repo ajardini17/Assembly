09c8a0fecf3c9ebda6fc97b7bda6b5495ccdfe07
EddieKeller <116ekg@gmail.com>
2017-08-30 19:19:33 -0700

    temp done with landing

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -79,39 +79,25 @@ export default class LandingPage extends React.Component {
 
         <div className="bgimg-3 w3-display-container w3-opacity-min">
           <div className="w3-display-middle">
-            <span className="w3-xxlarge w3-text-white w3-wide">GET STARTED</span>
+            <span className="w3-xxlarge w3-text-white w3-wide">WOOLFEY</span>
           </div>
         </div>
 
         <div className="w3-content w3-container w3-padding-64" id="contact">
-          <h3 className="w3-center">WHERE I WORK</h3>
-          <p className="w3-center"><em>I'd love your feedback!</em></p>
+          <h3 className="w3-center">Contact Us</h3>
+          <p className="w3-center"><em>We love your feedback!</em></p>
 
           <div className="w3-row w3-padding-32 w3-section">
-            <div className="w3-col m4 w3-container">
+            <div className="w3-col m4 w3-container w3-center">
               <div id="googleMap" className="w3-round-large w3-greyscale" style={{width:100 + '%', height:400 + 'px'}}></div>
             </div>
             <div className="w3-col m8 w3-panel">
               <div className="w3-large w3-margin-bottom">
-                <i className="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Chicago, US<br />
+                <i className="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Los Angeles, CA<br />
                 <i className="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Phone: +00 151515<br />
-                <i className="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Email: mail@mail.com<br />
+                <i className="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Email: contact@woolfey.com<br />
               </div>
-              <p>Swing by for a cup of <i className="fa fa-coffee"></i>, or leave me a note:</p>
-              <form action="/action_page.php" target="_blank">
-                <div className="w3-row-padding" style={{margin:0 -16 + 'px' + 8 + 'px' + -16 + 'px'}}>
-                  <div className="w3-half">
-                    <input className="w3-input w3-border" type="text" placeholder="Name" required name="Name" />
-                  </div>
-                  <div className="w3-half">
-                    <input className="w3-input w3-border" type="text" placeholder="Email" required name="Email" />
-                  </div>
-                </div>
-                <input className="w3-input w3-border" type="text" placeholder="Message" required name="Message" />
-                <button className="w3-button w3-black w3-right w3-section" type="submit">
-                  <i className="fa fa-paper-plane"></i> SEND MESSAGE
-                </button>
-              </form>
+              <p>Feel free to call or send us a message</p>
             </div>
           </div>
         </div>
