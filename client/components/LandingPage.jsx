24da58e9dcf31155a41b3fbed12693cdd42fa0d3
EddieKeller <116ekg@gmail.com>
2017-08-30 19:03:01 -0700

    fixing home page content

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -28,11 +28,12 @@ export default class LandingPage extends React.Component {
 
         
         <div className="w3-content w3-container w3-padding-64" id="about">
-          <h3 className="w3-center">TRADE CRYPTO STUFF</h3>
-          <p className="w3-center"><em>I love photography</em></p>
-          <p>We have created a fictional "personal" website/blog, and our fictional character is a hobby photographer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-            qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <h3 className="w3-center">FUTURE SIGHT</h3>
+          <p className="w3-center"><em>Make trades before anyone else</em></p>
+          <p>Using machine learning and sentiment analysis, we have developed a program that can predict the future prices of multiple 
+            crypto currencies, including Bitcoin, Ethereum, and Litecoin. Utilizing data from a number of resources, our FutureSight technology 
+            is able to make predictions with up to 99% accuracy.
+          </p>
           <div className="w3-row">
             <div className="w3-col m6 w3-center w3-padding-large">
               <img src="/images/chart.png" id='landingSmallChart' className="w3-round w3-image w3-opacity w3-hover-opacity-off" alt="Photo of Me" width="500" height="333" />
@@ -40,35 +41,38 @@ export default class LandingPage extends React.Component {
 
             
             <div className="w3-col m6 w3-hide-small w3-padding-large">
-              <p>Practice trading bitcoin, ethereum, and other crypto currencies</p>
+              <p>Get historical, real time, and predicted future data</p>
             </div>
           </div>
-          <p className="w3-large w3-center w3-padding-16">Improvement after using our site:</p>
-          <p className="w3-wide"><i className="fa fa-camera"></i>Bitcoin</p>
+          <p className="w3-large w3-center w3-padding-16">Accuracy of predictions:</p>
+          <p className="w3-wide"><i className="fa fa-btc"></i>Bitcoin</p>
           <div className="w3-light-grey">
             <div className="w3-container w3-padding-small w3-dark-grey w3-center" style={{width:90 + '%'}}>90%</div>
           </div>
-          <p className="w3-wide"><i className="fa fa-laptop"></i>Ethereum</p>
+          <p className="w3-wide"><i className="fa fa-etsy"></i>Ethereum</p>
           <div className="w3-light-grey">
-            <div className="w3-container w3-padding-small w3-dark-grey w3-center" style={{width:85 + '%'}}>85%</div>
+            <div className="w3-container w3-padding-small w3-dark-grey w3-center" style={{width:88 + '%'}}>88%</div>
           </div>
-          <p className="w3-wide"><i className="fa fa-photo"></i>Litecoin</p>
+          <p className="w3-wide"><i className="fa fa-usd"></i>Litecoin</p>
           <div className="w3-light-grey">
-            <div className="w3-container w3-padding-small w3-dark-grey w3-center" style={{width:75 + '%'}}>75%</div>
+            <div className="w3-container w3-padding-small w3-dark-grey w3-center" style={{width:90 + '%'}}>90%</div>
           </div>
         </div>
 
         <div className="bgimg-2 w3-display-container w3-opacity-min">
           <div className="w3-display-middle">
-            <span className="w3-xxlarge w3-text-white w3-wide">MACHINE LEARNING</span>
+            <span className="w3-xxlarge w3-text-white w3-wide">CRYPTO TRADER</span>
           </div>
         </div>
 
         <div className="w3-content w3-container w3-padding-64" id="portfolio">
-          <h3 className="w3-center">MY WORK</h3>
-          <p className="w3-center"><em>Here are some of my latest lorem work ipsum tipsum.
+          <h3 className="w3-center">MAKE MOVES</h3>
+          <p className="w3-center"><em>...without risking your money</em>
             <br /> 
-            Click on the images to make them bigger</em>
+              Use our crypto-currency trading simulator to practice your trades before you spend your actual money. 
+              Our simulator uses real time data for all crypto-currencies, allowing you to perform your trades the same as 
+              you would normally. You also have access to our FutureSight technology, giving you the edge in your trades. 
+              Be the best trader on Woolfey and reach the top of our leaderboards.
           </p>
           <br />
         </div>
