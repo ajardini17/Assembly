373104f8546d44ffe4eb5bac439ea6aa408bf563
EddieKeller <116ekg@gmail.com>
2017-09-10 20:37:32 -0700

    slight styling to leaderboard. moved down page and added highlight on hover

--- client/components/Leaderboard/Leaderboard.jsx
+++ client/components/Leaderboard/Leaderboard.jsx
@@ -25,27 +25,33 @@ export default class Leaderboard extends React.Component {
 
   render() {
     return (
-      <div>
-      <Navbar />
-      <table className='portfolioEntryTable'>
-        <caption id="table-caption">Leaderboards</caption>
-        <tbody>
-          <tr>
-            <th style={{ 'textAlign':'center' }}>Rank</th>
-            <th style={{ 'textAlign':'center' }}>Username</th>
-            <th style={{ 'textAlign':'center' }}>portfolio name</th>
-            <th style={{ 'textAlign':'center' }}>Value</th>
-          </tr>
+      <div className='container'>
+        <div className='row'>
+          <Navbar />
+        </div>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <table className='table-responsive table-hover leaderboardTable'>
+              <caption className='text-center' id="table-caption">Leaderboards</caption>
+              <tbody>
+                <tr>
+                  <th style={{ 'textAlign':'center' }}>Rank</th>
+                  <th style={{ 'textAlign':'center' }}>Username</th>
+                  <th style={{ 'textAlign':'center' }}>portfolio name</th>
+                  <th style={{ 'textAlign':'center' }}>Value</th>
+                </tr>
 
-          {this.state.entries.length > 0 ?
-          this.state.entries.map((item, index) => (
-            <LeaderboardEntry item={item} key={index} index={index}  /> 
-          ))
-          :
-          null
-          }
-        </tbody>
-      </table>
+                {this.state.entries.length > 0 ?
+                this.state.entries.map((item, index) => (
+                  <LeaderboardEntry item={item} key={index} index={index}  /> 
+                ))
+                :
+                null
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
 
     )
