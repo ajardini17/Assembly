b360ec43d9528ab7d81e267d2ee74d804d9d0986
EddieKeller <116ekg@gmail.com>
2017-09-11 19:16:05 -0700

    added tabs to leaderboards

--- client/components/Leaderboard/Leaderboard.jsx
+++ client/components/Leaderboard/Leaderboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LeaderboardEntry from './LeaderboardEntry.jsx'
 import Navbar from '../Navbar.js'
 import axios from 'axios'
+import {Tab, Tabs} from 'react-bootstrap'
 
 export default class Leaderboard extends React.Component {
   constructor(){
@@ -31,25 +32,38 @@ export default class Leaderboard extends React.Component {
         </div>
         <div className='row'>
           <div className='col-xs-12'>
-            <table className='table-responsive table-hover leaderboardTable'>
-              <caption className='text-center' id="table-caption">Leaderboards</caption>
-              <tbody>
-                <tr>
-                  <th style={{ 'textAlign':'center' }}>Rank</th>
-                  <th style={{ 'textAlign':'center' }}>Username</th>
-                  <th style={{ 'textAlign':'center' }}>portfolio name</th>
-                  <th style={{ 'textAlign':'center' }}>Value</th>
-                </tr>
+            <h1 style={{'marginTop':'100px'}} className='text-center'>Leaderboards</h1>
+            <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+              
+              <Tab eventKey={1} title="Total Value">
+                <table className='table-responsive table-hover leaderboardTable' style={{'marginTop':'10px'}}>
+                  <tbody>
+                    <tr>
+                      <th style={{ 'textAlign':'center' }}>Rank</th>
+                      <th style={{ 'textAlign':'center' }}>Username</th>
+                      <th style={{ 'textAlign':'center' }}>portfolio name</th>
+                      <th style={{ 'textAlign':'center' }}>Value</th>
+                    </tr>
 
-                {this.state.entries.length > 0 ?
-                this.state.entries.map((item, index) => (
-                  <LeaderboardEntry item={item} key={index} index={index}  /> 
-                ))
-                :
-                null
-                }
-              </tbody>
-            </table>
+                    {this.state.entries.length > 0 ?
+                    this.state.entries.map((item, index) => (
+                      <LeaderboardEntry item={item} key={index} index={index}  /> 
+                    ))
+                    :
+                    null
+                    }
+                  </tbody>
+                </table>
+              </Tab>
+              <Tab eventKey={2} title="Hourly Gain">
+                
+              </Tab>
+              <Tab eventKey={3} title='Monthly Gain'>
+
+              </Tab>
+
+            </Tabs>
+            
           </div>
         </div>
       </div>
